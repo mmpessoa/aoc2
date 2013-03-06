390e48c10bdb3ddc5d86273c24caca43a2facbad
Michelle Pessoa <mmpessoa@gmail.com>
2013-03-05 23:23:59 -0500

    Uploading test files for week one.
    These are "follow along with the video" files.

---
+++ week1/tests/accessTest/accessTest/BaseClass.h
@@ -0,0 +1,13 @@
+//
+//  BaseClass.h
+//  accessTest
+//
+//  Created by Michelle Pessoa on 3/5/13.
+//  Copyright (c) 2013 Michelle Pessoa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BaseClass : NSObject
+
+@end
---
+++ week1/tests/accessTest/accessTest/BaseClass.m
@@ -0,0 +1,13 @@
+//
+//  BaseClass.m
+//  accessTest
+//
+//  Created by Michelle Pessoa on 3/5/13.
+//  Copyright (c) 2013 Michelle Pessoa. All rights reserved.
+//
+
+#import "BaseClass.h"
+
+@implementation BaseClass
+
+@end
