86a6835d31cc269422695b97ea73cdb54f95b0b4
Michelle Pessoa <mmpessoa@gmail.com>
2013-03-05 23:25:57 -0500

    Third attempt to sync. Hmm.

---
+++ week1/tests/accessTest/accessTest/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  accessTest
+//
+//  Created by Michelle Pessoa on 3/5/13.
+//  Copyright (c) 2013 Michelle Pessoa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ week1/tests/accessTest/accessTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  accessTest
+//
+//  Created by Michelle Pessoa on 3/5/13.
+//  Copyright (c) 2013 Michelle Pessoa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
