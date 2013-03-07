57a8768fd3d9df8d565089a2c07d9b2deb5d98a6
Michelle Pessoa <mmpessoa@gmail.com>
2013-03-07 13:53:37 -0500

    Project 1

---
+++ week1/project1/project1/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  project1
+//
+//  Created by Michelle Pessoa on 3/7/13.
+//  Copyright (c) 2013 Michelle Pessoa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ week1/project1/project1/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  project1
+//
+//  Created by Michelle Pessoa on 3/7/13.
+//  Copyright (c) 2013 Michelle Pessoa. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
